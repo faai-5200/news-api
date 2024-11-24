3b831cfff2d41fe50f97bc4b0b757e68cda04d94
faai5200 <fakhar.faai@gmail.com>
2024-11-24 22:59:14 +0500

    Added fetch command, scheduler, article controller and service to fetch and store the news to local db.

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -3,16 +3,21 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        health: '/up'
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        // Define your scheduled tasks here
+        $schedule->command('news:fetch')->everyMinute();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ArticleController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,14 +15,25 @@ use App\Http\Controllers\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+Route::get('/articles', [ArticleController::class, 'getArticles']);
+Route::get('/articles/search', [ArticleController::class, 'searchArticles']);
+Route::get('/articles/{id}', [ArticleController::class, 'getArticleDetails']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/preferences', [UserPreferenceController::class, 'setPreferences']);
+    Route::get('/preferences', [UserPreferenceController::class, 'getPreferences']);
+    Route::get('/personalized-feed', [UserPreferenceController::class, 'personalizedFeed']);
+});
+
 Route::get('/test', function () {
     return response()->json(['message' => 'API is working']);
 });
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
 
 
 Route::post('/register', [AuthController::class, 'register']);
